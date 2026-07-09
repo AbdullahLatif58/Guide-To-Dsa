dc785c6511898188a25f7bf5d7b9a399104dee59
webs <22011556-068@uog.edu.pk>
2026-07-09 12:52:19 +0500

    arr

--- src/dsa/modules/advanceArray/oop.ts
+++ src/dsa/modules/advanceArray/oop.ts
@@ -1,115 +1,175 @@
-// // left rotation of one element in an array
+// // // left rotation of one element in an array
 
 
-let arr = [1,2,3,4,5] as any;
+// let arr = [1,2,3,4,5] as any;
   
-  let copy = arr[0];
-   for(let i = 0; i < arr.length - 1; i++) {
+//   let copy = arr[0];
+//    for(let i = 0; i < arr.length - 1; i++) {
      
-      arr[i] = arr[i + 1];
+//       arr[i] = arr[i + 1];
    
-   }
+//    }
 
 
-   arr[arr.length - 1]  = copy;
+//    arr[arr.length - 1]  = copy;
 
-   console.log(arr);
+//    console.log(arr);
 
-   // output: [ 2, 3, 4, 5, 1 ]
+//    // output: [ 2, 3, 4, 5, 1 ]
 
-   // Easy way to do this same example  using shift
+//    // Easy way to do this same example  using shift
 
-   const first = arr.shift();
+//    const first = arr.shift();
 
-    if(first !== undefined) {
-      arr.push(first);
-    }
-    console.log(arr);
+//     if(first !== undefined) {
+//       arr.push(first);
+//     }
+//     console.log(arr);
 
 
 
-    // Left and right rotation of an array by n elements
+//     // Left and right rotation of an array by n elements
 
 
   
   
 
 
-  for (let j = 0; j < 2; j++) {
-     let copy = arr[0];
+//   for (let j = 0; j < 2; j++) {
+//      let copy = arr[0];
       
-      for(let i = 0; i < arr.length - 1; i++) {
+//       for(let i = 0; i < arr.length - 1; i++) {
      
-      arr[i] = arr[i + 1];
+//       arr[i] = arr[i + 1];
    
-   }
-     arr[arr.length - 1]  = copy;
-  }
+//    }
+//      arr[arr.length - 1]  = copy;
+//   }
  
 
   
-   console.log(arr);
+//    console.log(arr);
 
 
-// advance way of doing left rotation of an array by n elemmnts 
-  for (let index = 0; index < 6; index++) {
+// // advance way of doing left rotation of an array by n elemmnts 
+//   for (let index = 0; index < 6; index++) {
    
-    const first = arr.shift();
+//     const first = arr.shift();
 
-  if(first !== undefined ) {
-      arr.push(first);
+//   if(first !== undefined ) {
+//       arr.push(first);
      
-  }
+//   }
 
-  }
-  console.log(arr);
-   // 3, 4,5, 1, 2
+//   }
+//   console.log(arr);
+//    // 3, 4,5, 1, 2
 
 
-  // left rotation of an array by one element
+//   // left rotation of an array by one element
    
    
-  let copys= arr[arr.length - 1];
-   for(let i = arr.length - 1; i > 0; i--) {
-      console.log(arr[i]);
-      arr[i] = arr[i - 1];
+//   let copys= arr[arr.length - 1];
+//    for(let i = arr.length - 1; i > 0; i--) {
+//       console.log(arr[i]);
+//       arr[i] = arr[i - 1];
 
-   }
+//    }
 
-   arr[0]  = copys;
+//    arr[0]  = copys;
 
-   console.log(arr);
+//    console.log(arr);
 
-   // ouput: [ 5, 1, 2, 3, 4 ]
+//    // ouput: [ 5, 1, 2, 3, 4 ]
 
 
-   // left rotation nested loop of an array by n elements;
+//    // left rotation nested loop of an array by n elements;
    
 
    
-   for(let j = 0; j < 2; j++) {
-      let copy = arr[arr.length - 1];
-      console.log(copy + " copy");
+//    for(let j = 0; j < 2; j++) {
+//       let copy = arr[arr.length - 1];
+    
       
 
-      for (let i = arr.length - 1; i > 0; i--) {
+//       for (let i = arr.length - 1; i > 0; i--) {
       
-      console.log("arrat length in inner loop " + i);
+
 
       
-         arr[i] = arr[i - 1]; 
+//          arr[i] = arr[i - 1]; 
        
-        console.log("array index in inner loop: " + arr[i]);
-        console.log(arr);
-      }
-      arr[0] = copy;
-   }
+       
 
-   console.log(arr);
+//       }
+//       arr[0] = copy;
+//    }
 
+//    console.log(arr);
 
+//    //output:  [4,5,1,2,3]
 
+//    let arrays = [1,2,3,4,5] as any;
 
+//    for(let j = 0; j < 2; j++) {
+//       let last = arrays.pop();
+//       if(last !== undefined){
+//          arrays.unshift(last);
+//       }
+//    }
 
+//    console.log(arrays);
 
-   //output:  [4,5,1,2,3]
+// //output:  [4, 5, 1, 2, 3]
+
+// // advance way of left rotation of an array by n elements using reverse method
+
+// let array = [1,2,3,4,5] as any;
+
+// let k = 2;
+// k = k % array.length;
+
+//  reverse(0, k -1)
+
+ 
+
+//    function  reverse(i: any , j: any): void {
+//       while( i < j ) {
+//          let temp = array[i];
+//          console.log("temp is " +  temp);
+//          array[i] = array[j];
+//          array[j] = temp;
+//          console.log( temp);
+//          i++
+//          console.log(1); 
+//          j--
+//          console.log(j);
+//       }
+//    }
+// console.log(array);
+
+// output: [2, 1, 3, 4, 5]
+
+// getting eliminating the duplication in an sorted array
+const arrayss = [1,1,2,3,4,5,5,5] as any;
+removeDuplication(arrayss)
+function removeDuplication(nums: number[]) {
+
+    let j = 1;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+
+        if (nums[i] !== nums[i + 1]) {
+
+            nums[j]  = nums[i + 1] as any;
+
+            j++;
+        }
+
+    }
+
+    return j;
+}
+ 
+
+ console.log(arrayss);
